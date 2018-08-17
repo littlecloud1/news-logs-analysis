dbb2653afa07349c8e89072c4f8b951a9251b28a
littlecloud1 <cloudtang030@gmail.com>
2018-08-17 22:37:00 +0800

    change to PEP8 style

--- newsdb.py
+++ newsdb.py
@@ -1,41 +1,69 @@
-''' Database code for 
-    author: Lai Man Tang(Nancy) 
+#!/usr/bin/env python3
+''' Database code for
+    author: Lai Man Tang(Nancy)
     email: cloudtang030@gmail.com
 '''
 
-import psycopg2 
+import psycopg2
 
 '''
-create view articlesLog as 
+create view articlesLog as
 select au.name as author, a.title, l.id as logID, l.status as logStatus
-from log as l, articles as a, authors as au 
+from log as l, articles as a, authors as au
 where l.path like concat('%/', a.slug) and au.id = a.author;
 
 '''
 
+
 def popular_articles(nums=3):
-	""" return articles have been accessed the most. default return 3 articles."""
-	db = psycopg2.connect(dbname='news')
-	c = db.cursor()
-	c.execute("select title ,count(title) as views from articlesLog group by title order by count(title) desc limit (%s);",(nums,))
-	msg = c. fetchall()
-	db.close()
-	return msg
+    """ return articles have been accessed the most.
+    default return 3 articles."""
+    db = psycopg2.connect(dbname='news')
+    c = db.cursor()
+    c.execute("""
+        SELECT title,
+               count(title) AS views
+        FROM articlesLog
+        GROUP BY title
+        ORDER BY count(title) DESC
+        LIMIT (%s);
+        """, (nums,))
+    msg = c. fetchall()
+    db.close()
+    return msg
+
 
 def popular_authors():
-	""" return the pages views each authors have recieved."""
-	db = psycopg2.connect(dbname='news')
-	c = db.cursor()
-	c.execute("select author ,count(author) as views from articlesLog group by author order by count(author) desc;")
-	msg = c. fetchall()
-	db.close()
-	return msg
+    """ return the pages views each authors have recieved."""
+    db = psycopg2.connect(dbname='news')
+    c = db.cursor()
+    c.execute('''
+        SELECT author,
+               count(author) AS views
+        FROM articlesLog
+        GROUP BY author
+        ORDER BY count(author) desc;
+        ''')
+    msg = c. fetchall()
+    db.close()
+    return msg
+
 
 def request_error():
-	""" return the pages views each authors have recieved."""
-	db = psycopg2.connect(dbname='news')
-	c = db.cursor()
-	c.execute(" select date(time) as date, count(*) as total ,count(case when status like '4%' then 1 else null end) as error from log group by date(time) order by date(time);")
-	msg = c. fetchall()
-	db.close()
-	return msg
+    """ return the pages views each authors have recieved."""
+    db = psycopg2.connect(dbname='news')
+    c = db.cursor()
+    c.execute("""
+        SELECT  date(TIME) AS date,
+                count(*) AS total,
+                count(CASE
+                         WHEN status LIKE '4%' THEN 1
+                         ELSE NULL
+                      END)::float AS error
+        FROM log
+        GROUP BY date(TIME)
+        ORDER BY date(TIME);
+        """)
+    msg = c. fetchall()
+    db.close()
+    return msg
